5a1c9f79d99a296b6d64231c0f0f500ba0df0979
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-05-19 09:19:58 +0100

    Updating cookoff

--- packages/GreatCMakeCookoff/package.py
+++ packages/GreatCMakeCookoff/package.py
@@ -6,6 +6,7 @@ class Greatcmakecookoff(Package):
     homepage = "https://github.com/UCL/GreatCMakeCookOff"
     url      = "https://github.com/UCL/GreatCMakeCookOff/archive/v2.1.1.tar.gz"
 
+    version('2.1.7', '7882016d951b591ddebd3135d66a4ae82e97c90a')
     version('2.1.6', '6685508c7e452279e5374a4492cc6312a1bda223')
     version('2.1.5', "1afedb3896a19247156aa650080d68169cc9cb21")
     version('2.1.3', "07800bce1900b4fbe1160baf293e73d8ca3851be")
