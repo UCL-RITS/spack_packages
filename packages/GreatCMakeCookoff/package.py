9889560b40e210f9d58b6f77f6aa7c22fdca9000
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-05-19 10:11:05 +0100

    Updating sha for cookoff

--- packages/GreatCMakeCookoff/package.py
+++ packages/GreatCMakeCookoff/package.py
@@ -6,7 +6,7 @@ class Greatcmakecookoff(Package):
     homepage = "https://github.com/UCL/GreatCMakeCookOff"
     url      = "https://github.com/UCL/GreatCMakeCookOff/archive/v2.1.1.tar.gz"
 
-    version('2.1.7', '0f2af642ac6c0e94fa8d340bbefc60f22e093d46')
+    version('2.1.7', '036b02886055e1d084516ccac0e6eacb821ba059')
     version('2.1.6', '6685508c7e452279e5374a4492cc6312a1bda223')
     version('2.1.5', "1afedb3896a19247156aa650080d68169cc9cb21")
     version('2.1.3', "07800bce1900b4fbe1160baf293e73d8ca3851be")
